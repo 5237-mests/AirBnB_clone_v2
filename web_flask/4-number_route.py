6860de7fad25443d0b33e55033dab2a7d8131094
5237-mests <msfnmulgeta@gmail.com>
2022-10-15 01:01:05 +0300

    jinja2

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application:
+"""
+
+from email.policy import default
+from tkinter.tix import INTEGER
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display1():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display2():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display3(text):
+    return 'C ' + str(text).replace("_", " ")
+
+
+@app.route('/python/<text2>', strict_slashes=False)
+def display4(text2):
+    return 'Python ' + str(text2).replace("_", " ")
+
+
+@app.route('/python/', defaults={'text3': 'is cool'},  strict_slashes=False)
+def display5(text3):
+    return 'Python ' + str(text3).replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display6(n):
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application:
+"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display1():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display2():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display3(text):
+    return 'C ' + str(text).replace("_", " ")
+
+
+@app.route('/python/<text2>', strict_slashes=False)
+def display4(text2):
+    return 'Python ' + str(text2).replace("_", " ")
+
+
+@app.route('/python/', defaults={'text3': 'is cool'},  strict_slashes=False)
+def display5(text3):
+    return 'Python ' + str(text3).replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display6(n):
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display7(n):
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
